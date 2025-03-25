bc400fdfb12f40a5558845e6de5889f5152fa220
Gbrvi <gv.almeida06@gmail.com>
2025-03-25 18:13:44 -0300

    Continuous message + Reply interface

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ class Client():
         self.dealer = self.context.socket(zmq.DEALER)
         self.dealer.connect("tcp://localhost:5555")
         self.clientID = None
+        self.senderID = None
 
     def connect(self):
         self.clientID = input("Enter your ID: ").strip()
@@ -13,24 +14,40 @@ class Client():
         self.dealer.send_multipart([b"", f"REGISTER:{self.clientID}".encode()])  
 
     def receive_messages(self):
+        '''
+        NOTE: If you're in a conversation, and you got a new user sending a message to you, the next message will reply THIS person
+        '''
         while True:
             try:
-                _, message = self.dealer.recv_multipart()
-                print(f"\n[New message] {message.decode()}\nYou: ", end="", flush=True)
+                _, message = self.dealer.recv_multipart() # Get the message
+                message_decoded = message.decode() # Decoded the message
+                self.senderID = message_decoded.split(":")[0].strip() # Get the senderID format client1:message
+                print(f"\n[New message from {self.senderID}] {message_decoded}", end="", flush=True) #Show up the message
+
+                print(f"Reply to {self.senderID} > ", end="", flush=True) #Reply to the person
             except zmq.ZMQError:
                 break
+
+def set_recipientID(): # Set the recipient ID (Who you gonna send the message)
+    return input("Send to (ID): ").strip()
 
 def main():
     client = Client()
     client.connect()
 
     # Start receiver thread
-    threading.Thread(target=client.receive_messages, daemon=True).start()
+    threading.Thread(target=client.receive_messages, daemon=True).start() 
+
+    recipient_id = set_recipientID()
 
     while True:
-        recipient_id = input("Send to (ID): ").strip()
-        msg = input("Message: ").strip()
-        client.dealer.send_multipart([b"", f"{recipient_id}:{msg}".encode()])
+        msg = input(f"Message to {recipient_id}:  ([-1] to change contact) ").strip()
+
+        if(msg == "-1"):
+            recipient_id = set_recipientID()
+            continue # restart the loop 
+
+        client.dealer.send_multipart([b"", f"{recipient_id}:{msg}".encode()]) # Send message
 
 if __name__ == "__main__":
     main()
